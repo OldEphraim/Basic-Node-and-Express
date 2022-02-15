634998e1f6295f424422c13b7cc98ee77cd66d70
oldephraim <a8garber@yahoo.com>
2022-02-14 23:47:48 -0600

    saving progress

--- myApp.js
+++ myApp.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-
+app.get("/json", function(req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({"message": "HELLO JSON"});
+  } else {
+    res.json({"message": "Hello json"});
+  };
+});
 
 
 
