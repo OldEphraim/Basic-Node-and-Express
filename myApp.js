d63483fe3f4e94ce32e042096e2ddae5f70447ca
oldephraim <a8garber@yahoo.com>
2022-02-16 01:03:41 -0600

    finished with this

--- myApp.js
+++ myApp.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 require('dotenv').config();
+app.use(function(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
@@ -14,29 +22,22 @@ app.get("/json", function(req, res) {
   };
 });
 
+app.get("/now", function(req, res, next) {
+    res.json({"time": new Date().toString()});
+    next;
+}, function(req, res) {
+});
 
+app.get("/:word/echo", function(req, res) {
+  res.json({"echo": req.params.word});
+});
 
+app.get("/name", function(req, res) {
+  res.json({"name": req.query.first + " " + req.query.last});
+});
 
+app.post("/name", function(req, res) {
+  res.json({"name": req.body.first + " " + req.body.last});
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
